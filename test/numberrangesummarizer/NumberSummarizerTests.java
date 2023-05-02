221a15498b6630eb9ff119b1181ebe7fd7d81ffa
baden-dev <baden.dev@gmail.com>
2023-05-03 00:05:55 +0200

    added test condition for collect method when input is not in sequential order

--- test/numberrangesummarizer/NumberSummarizerTests.java
+++ test/numberrangesummarizer/NumberSummarizerTests.java
@@ -1,6 +1,7 @@
 package numberrangesummarizer;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -8,6 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,53 +21,66 @@ class NumberSummarizerTests {
         numberSummarizer = new NumberSummarizer();
     }
 
-    @Test
-    void collect_inputPositiveNumbersValid_returnPopulatedCollection() {
-        String input = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
-        Collection<Integer> expectedOutput =
-                new ArrayList<>(Arrays.asList(1,3,6,7,8,12,13,14,15,21,22,23,24,31));
-        Collection<Integer> actualOutput = numberSummarizer.collect(input);
-        assertArrayEquals(expectedOutput.toArray(), actualOutput.toArray());
+    @Nested
+    class CollectMethodTests {
+
+        @Test
+        void collect_inputNotInOrder_returnPopulatedCollection() {
+            String input = "5,4,3,2,2";
+            Collection<Integer> expectedOutput =
+                    new ArrayList<>(Arrays.asList(5,4,3,2));
+            Collection<Integer> actualOutput = numberSummarizer.collect(input);
+            assertArrayEquals(expectedOutput.toArray(), actualOutput.toArray());
+        }
+        @Test
+        void collect_inputPositiveNumbersValid_returnPopulatedCollection() {
+            String input = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
+            Collection<Integer> expectedOutput =
+                    new ArrayList<>(Arrays.asList(1,3,6,7,8,12,13,14,15,21,22,23,24,31));
+            Collection<Integer> actualOutput = numberSummarizer.collect(input);
+            assertArrayEquals(expectedOutput.toArray(), actualOutput.toArray());
+        }
+
+        @Test
+        void collect_inputNegativeNumbersValid_returnPopulatedCollection() {
+            String input = "-7,-6,-4,-2,-1,0,1,2,5,7";
+            Collection<Integer> expectedOutput =
+                    new ArrayList<>(Arrays.asList(-7,-6,-4,-2,-1,0,1,2,5,7));
+            Collection<Integer> actualOutput = numberSummarizer.collect(input);
+
+            assertArrayEquals(expectedOutput.toArray(), actualOutput.toArray());
+        }
+
+        @Test
+        void collect_inputDoubleNumbersValid_returnPopulatedCollection() {
+            String expectedOutput = "[1, 3, 4, 7, 8, 9]";
+            Collection<Integer> actualOutput = numberSummarizer.collect("1, 3, 3, 4, 7, 8, 9");
+
+            assertEquals(expectedOutput, actualOutput.toString());
+            assertEquals(6, actualOutput.size());
+            assertEquals(1, Arrays.stream(actualOutput.toArray()).filter(i -> i.equals(3)).count());
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"3", "3, 3, 3"})
+        void collect_inputSingleValue_returnCollectionSizeOne(String input) {
+            Collection<Integer> actualOutput =  numberSummarizer.collect(input);
+            assertEquals(1, actualOutput.size());
+        }
+
+        @Test
+        void collect_inputEmptyString_throwException() {
+            assertThrows(NumberFormatException.class,
+                    () -> numberSummarizer.collect(""),
+                    "This should have thrown a NumberFormatException");
+        }
+
+        @Test
+        void collect_inputNonNumericValue_throwException() {
+            assertThrows(NumberFormatException.class,
+                    () -> numberSummarizer.collect("1,b,6,7,8,12,13"),
+                    "This should have thrown a NumberFormatException");
+        }
     }
 
-    @Test
-    void collect_inputNegativeNumbersValid_returnPopulatedCollection() {
-        String input = "-7,-6,-4,-2,-1,0,1,2,5,7";
-        Collection<Integer> expectedOutput =
-                new ArrayList<>(Arrays.asList(-7,-6,-4,-2,-1,0,1,2,5,7));
-        Collection<Integer> actualOutput = numberSummarizer.collect(input);
-
-        assertArrayEquals(expectedOutput.toArray(), actualOutput.toArray());
-    }
-
-    @Test
-    void collect_inputDoubleNumbersValid_returnPopulatedCollection() {
-        String expectedOutput = "[1, 3, 4, 7, 8, 9]";
-        Collection<Integer> actualOutput = numberSummarizer.collect("1, 3, 3, 4, 7, 8, 9");
-
-        assertEquals(expectedOutput, actualOutput.toString());
-        assertEquals(6, actualOutput.size());
-        assertEquals(1, Arrays.stream(actualOutput.toArray()).filter(i -> i.equals(3)).count());
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"3", "3, 3, 3"})
-    void collect_inputSingleValue_returnCollectionSizeOne(String input) {
-        Collection<Integer> actualOutput =  numberSummarizer.collect(input);
-        assertEquals(1, actualOutput.size());
-    }
-
-    @Test
-    void collect_inputEmptyString_throwException() {
-        assertThrows(NumberFormatException.class,
-                () -> numberSummarizer.collect(""),
-                "This should have thrown a NumberFormatException");
-    }
-
-    @Test
-    void collect_inputNonNumericValue_throwException() {
-        assertThrows(NumberFormatException.class,
-                () -> numberSummarizer.collect("1,b,6,7,8,12,13"),
-                "This should have thrown a NumberFormatException");
-    }
 }
