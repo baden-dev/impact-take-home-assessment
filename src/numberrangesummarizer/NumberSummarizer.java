abf78b176377085049360cc158c17973caaa5986
baden-dev <baden.dev@gmail.com>
2023-05-02 23:59:53 +0200

    Implemented code logic for collect method (all tests for method pass)

--- src/numberrangesummarizer/NumberSummarizer.java
+++ src/numberrangesummarizer/NumberSummarizer.java
@@ -1,12 +1,23 @@
 package numberrangesummarizer;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class NumberSummarizer implements NumberRangeSummarizer{
 
     @Override
     public Collection<Integer> collect(String input) {
-        return null;
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .distinct()
+                .map(s -> {
+                    try {
+                        return Integer.parseInt(s);
+                    } catch (NumberFormatException e) {
+                        throw new NumberFormatException("\"" + s + "\" is not a valid integer");
+                    }
+                }).collect(Collectors.toList());
     }
 
     @Override
